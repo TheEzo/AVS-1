747e8d381978edcd517e4741c37ed4c77bdd137e
TheEzo <willaschek.t@gmail.com>
2019-11-08 19:26:20 +0100

    asdf

---
+++ cmake-build-debug/Step0/CMakeFiles/ANN_0.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "ANN"
+  "ANN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ANN_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
