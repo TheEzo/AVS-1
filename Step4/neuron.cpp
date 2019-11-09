3f6965f1d1c234e6406b8fbc5ed3321351a5db19
TheEzo <willaschek.t@gmail.com>
2019-11-10 00:44:12 +0100

    step4

---
+++ Step4/neuron.cpp
@@ -0,0 +1,23 @@
+/*
+ * Architektury výpočetních systémů (AVS 2019)
+ * Projekt c. 1 (ANN)
+ * Login: xwilla00
+ */
+
+#include <cstdlib>
+#include "neuron.h"
+
+float evalNeuron(
+        size_t inputSize,
+        const float* input,
+        const float* weights,
+        float bias
+)
+{
+    float result = 0;
+    #pragma omp simd reduction(+:result)
+    for(int i = 0; i < inputSize; i++)
+        result += input[i] * weights[i];
+    result += bias;
+    return result > 0 ? result : 0;
+}
